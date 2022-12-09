65aa5db28747c5a9534e9fe819e79e4c21a3350b
Enrico Pittini <pittinienrico@hotmail.it>
2022-12-09 11:15:19 +0100

    Update training.py

--- utils/training.py
+++ utils/training.py
@@ -1,3 +1,4 @@
+import os
 import torch 
 import time
 
@@ -5,7 +6,9 @@ import matplotlib.pyplot as plt
 
 
 
-def train(train_dataloader, model, epochs=(2,1,0), steps_per_update=2, steps_empty_cache=None, device : str ='cpu', plot=False):
+def train(train_dataloader, model, model_name, epochs=(2,1,0), optimizers=None, learnin_rates=None, steps_per_update=2, 
+          steps_empty_cache=None, parameters_file_end='', device : str ='cpu', plot=False):
+
     token_importances_extractor = model.token_importances_extractor
     encoder_decoder = model.encoder_decoder
     tokenizer = model.tokenizer
@@ -14,43 +17,72 @@ def train(train_dataloader, model, epochs=(2,1,0), steps_per_update=2, steps_emp
     epochs2 = epochs[1]
     epochs3 = epochs[2]
 
-    loss_history1, optim1 = train_tokenImportancesExtractor(train_dataloader, token_importances_extractor, tokenizer, 
-                                                  epochs=epochs1, 
-                                                  learning_rate=1e-5,
-                                                  steps_per_update=steps_per_update, steps_empty_cache=steps_empty_cache,
-                                                  device=device)
+    if optimizers is not None:
+        optim1 = optimizers[0]
+        optim2 = optimizers[1]
+        optim3 = optimizers[2]
+    else:
+        optim1, optim2, optim3 = None, None, None
+
+    if learnin_rates is not None:
+        lr1 = learnin_rates[0]
+        lr2 = learnin_rates[1]
+        lr3 = learnin_rates[2]
+    else:
+        lr1, lr2, lr3 = None, None, None
+
+    loss_history1 = train_tokenImportancesExtractor(train_dataloader, token_importances_extractor, tokenizer, model_name,
+                                                    epochs=epochs1, optimizer=optim1, learning_rate=lr1, 
+                                                    steps_per_update=steps_per_update, 
+                                                    steps_empty_cache=steps_empty_cache, parameters_file_end=parameters_file_end,
+                                                    device=device)
     if plot and epochs1>0:
         plt.plot(loss_history1)
         plt.xlabel('Epochs')
         plt.title('Training history first phase')
 
-    loss_history2, optim2 = train_EncoderDecoder(train_dataloader, token_importances_extractor, encoder_decoder, tokenizer, 
-                                                 epochs=epochs2, 
-                         learning_rate=1e-5, steps_per_update=steps_per_update,
-                         steps_empty_cache=steps_empty_cache, device=device)
+    # torch.cuda.empty_cache()
+
+    loss_history2 = train_EncoderDecoder(train_dataloader, token_importances_extractor, encoder_decoder, tokenizer, model_name,
+                                         epochs=epochs2, optimizer=optim2, learning_rate=lr2, 
+                                         steps_per_update=steps_per_update, steps_empty_cache=steps_empty_cache, 
+                                         parameters_file_end=parameters_file_end,
+                                         device=device)
     if plot and epochs2>0:
         plt.plot(loss_history2)
         plt.xlabel('Epochs')
         plt.title('Training history second phase')
 
-    loss_history3, optim3 = train_EncoderDecoder(train_dataloader, token_importances_extractor, encoder_decoder, tokenizer, epochs=epochs3, 
-                                       learning_rate=1e-5, 
-                                       train_tokenImportancesExtractor=True, 
-                                       steps_per_update=steps_per_update, steps_empty_cache=steps_empty_cache, device=device)
+    # torch.cuda.empty_cache()
+
+    loss_history3 = train_EncoderDecoder(train_dataloader, token_importances_extractor, encoder_decoder, tokenizer, model_name,
+                                         train_tokenImportancesExtractor=True, epochs=epochs3, optimizer=optim3, 
+                                         learning_rate=lr3, steps_per_update=steps_per_update, 
+                                         steps_empty_cache=steps_empty_cache, device=device)
     if plot and epochs3>0:
         plt.plot(loss_history3)
         plt.xlabel('Epochs')
         plt.title('Training history third phase')
 
+    # torch.cuda.empty_cache()
 
+
+
+def _save_model_parameters(model, model_name, model_type, suffix=''):
+    os.makedirs('./weigths', exist_ok=True)
+    file_name = f'{model_name}_{model_type}_{suffix}.pt'
+    file_path = os.path.join('./weigths', file_name)
+    torch.save(model.state_dict(), file_path)
 
 def loss_func_tokenImportancesExtractor(probs, target):
     loss=  - torch.log(  probs) * (  target) / (torch.sum(  target, 1, keepdim=True)+1)
     loss+= - torch.log(1-probs) * (1-target) / (torch.sum(1-target, 1, keepdim=True)+1)
     return torch.mean(torch.sum(loss,(1,2)))
 
-def train_tokenImportancesExtractor(train_dataloader, token_importances_extractor, tokenizer, epochs=1, learning_rate=1e-5, 
-                                    optimizer=None, loss_history=[], steps_per_update=1, steps_empty_cache=None, 
+def train_tokenImportancesExtractor(train_dataloader, token_importances_extractor, tokenizer, model_name, epochs=1, 
+                                    optimizer=None,
+                                    learning_rate=1e-5, loss_history=[], steps_per_update=1, steps_empty_cache=None, 
+                                    parameters_file_end='',
                                     device : str = 'cpu'):
     
     #token_importances_extractor.to('cuda')
@@ -121,16 +153,19 @@ def train_tokenImportancesExtractor(train_dataloader, token_importances_extracto
             epoch_time = time.time() - start_time
             batch_time = epoch_time/(batch_idx+1)
 
-            print(f"epoch: {epoch + 1}/{epochs}, {batch_idx + 1}/{len(train_dataloader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, lr: {optimizer.param_groups[0]['lr']:.3g}, loss: {running_loss/(batch_idx+1):.3g}               ")#, end = '\r'
+            print(f"epoch: {epoch + 1}/{epochs}, {batch_idx + 1}/{len(train_dataloader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, lr: {optimizer.param_groups[0]['lr']:.3g}, loss: {running_loss/(batch_idx+1):.3g}", end = '\r')
 
-        print(f"epoch: {epoch + 1}/{epochs}, {batch_idx + 1}/{len(train_dataloader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, lr: {optimizer.param_groups[0]['lr']:.3g}, loss: {running_loss/(batch_idx+1):.3g}              ")
+            _save_model_parameters(model=token_importances_extractor, model_name=model_name, model_type='TokenImportancesExtractor',
+                                suffix=parameters_file_end)
 
-    return loss_history, optimizer
+        print(f"epoch: {epoch + 1}/{epochs}, {batch_idx + 1}/{len(train_dataloader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, lr: {optimizer.param_groups[0]['lr']:.3g}, loss: {running_loss/(batch_idx+1):.3g}")
+
+    return loss_history
 
 
-def train_EncoderDecoder(train_dataloader, token_importances_extractor, encoder_decoder, tokenizer, epochs=3, 
-                         learning_rate=1e-5, optimizer=None, loss_history=[], train_tokenImportancesExtractor=False, 
-                         steps_per_update=1, steps_empty_cache=None, device : str = 'cpu'):
+def train_EncoderDecoder(train_dataloader, token_importances_extractor, encoder_decoder, tokenizer, model_name,
+                         train_tokenImportancesExtractor=False, epochs=3, optimizer=None, learning_rate=1e-5, loss_history=[],  
+                         steps_per_update=1, steps_empty_cache=None, parameters_file_end='', device : str = 'cpu'):
 
     #token_importances_extractor.to('cuda')
     #encoder_decoder.to('cuda')
@@ -203,8 +238,14 @@ def train_EncoderDecoder(train_dataloader, token_importances_extractor, encoder_
             epoch_time = time.time() - start_time
             batch_time = epoch_time/(batch_idx+1)
             
-            print(f"epoch: {epoch + 1}/{epochs}, {batch_idx + 1}/{len(train_dataloader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, lr: {optimizer.param_groups[0]['lr']:.3g}, loss: {running_loss/(batch_idx+1):.3g}")
+            print(f"epoch: {epoch + 1}/{epochs}, {batch_idx + 1}/{len(train_dataloader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, lr: {optimizer.param_groups[0]['lr']:.3g}, loss: {running_loss/(batch_idx+1):.3g}", end='\r')
+
+            _save_model_parameters(model=encoder_decoder, model_name=model_name, model_type='EncoderDecoder', 
+                                   suffix=parameters_file_end)
+            if train_tokenImportancesExtractor:
+                _save_model_parameters(model=token_importances_extractor, model_name=model_name, model_type='TokenImportancesExtractor', 
+                                       suffix=parameters_file_end)
 
         print(f"epoch: {epoch + 1}/{epochs}, {batch_idx + 1}/{len(train_dataloader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, lr: {optimizer.param_groups[0]['lr']:.3g}, loss: {running_loss/(batch_idx+1):.3g}")
 
-    return loss_history, optimizer
+    return loss_history
