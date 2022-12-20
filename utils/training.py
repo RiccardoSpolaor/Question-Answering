b0411bac3ee768e2958f2c4f9862cbde4f5654f6
samuele-bortolato <samuele.bortolato@gmail.com>
2022-12-20 16:35:13 +0100

    added checkpoints

--- utils/training.py
+++ utils/training.py
@@ -298,6 +298,178 @@ def train_EncoderDecoder(train_dataloader, token_importances_extractor, encoder_
     return loss_history
 
 
+def train_1(train_dataloader, val_dataloader, model, use_history=False,
+            epochs=3, learning_rate=5e-5, opt_state_dict=None, 
+            steps_per_update=1, steps_empty_cache=None, steps_validate=None, steps_save=None,
+            loss_history=None, val_loss_history=None, seed=None, device ='cpu', plot=False):
+
+    model_name=model.model_name.replace('/','_')
+
+    if use_history:
+        folder_name = 'weigths\PQH\seed'+str(seed)
+    else:
+        folder_name = 'weigths\PQ\seed'+str(seed)
+    os.makedirs(folder_name, exist_ok=True)
+
+    if loss_history is None:
+        loss_history=[]
+        val_loss_history=[]
+
+    token_importances_extractor = model.token_importances_extractor
+    encoder_decoder = model.encoder_decoder
+    tokenizer = model.tokenizer
+
+
+    optimizer = torch.optim.Adam(iter(list(token_importances_extractor.parameters())+list(encoder_decoder.parameters())), 
+                                        lr=learning_rate)
+    if opt_state_dict is not None:
+        optimizer.load_state_dict(opt_state_dict)
+
+    tot_steps=len(train_dataloader)*epochs
+    n_steps=0
+    skip_steps=len(loss_history)
+
+    for epoch in range(epochs):  # loop over the dataset multiple times
+        
+        #times=display('', display_id=True)
+        disp=display('', display_id=True)
+
+        torch.cuda.empty_cache()
+        running_loss1 = 0.0
+        running_loss2 = 0.0
+        optimizer.zero_grad()
+        start_time = time.time()
+        batch_steps=0
+        for batch_idx, data in enumerate(train_dataloader, 0):
+
+            if n_steps<skip_steps:   #necessary to restart stopped training mantaining the seed
+                n_steps+=1
+                continue
+            n_steps+=1
+            batch_steps+=1
+
+            # get the inputs; data is a list of [inputs, labels]
+            (passage, question, history), (answer, sep_starts, sep_ends) = data
+            history = tuple([h.split(' <sep> ') for h in history])
+
+            if use_history:
+                separator = f' {tokenizer.sep_token} '
+                question = tuple([q + f'{separator if len(h) else ""}' + separator.join(h) for q, h in zip(question, history)])
+            
+
+            inputs = tokenizer(
+                question,
+                passage,
+                max_length=512,
+                truncation=True,
+                padding=True,
+                return_tensors="pt",
+            ).to(device)
+
+            labels = tokenizer(
+                list(answer),
+                max_length=512,
+                truncation=True,
+                padding=True,
+                return_tensors="pt",
+            ).to(device)
+
+            y = torch.zeros(inputs.input_ids.shape+(1,), device=device)
+
+            for i in range(len(sep_starts)):
+                
+                start_tok = inputs.char_to_token(i,sep_starts[i],1)
+                end_tok = inputs.char_to_token(i,sep_ends[i],1)
+
+                if start_tok is None:
+                    start_tok = inputs.char_to_token(i,sep_starts[i]+1,1)
+                if start_tok is None:
+                    start_tok = inputs.char_to_token(i,sep_starts[i]-1,1)
+
+                if end_tok is None:
+                    end_tok = inputs.char_to_token(i,sep_ends[i]-1,1)
+                if end_tok is None:
+                    end_tok = inputs.char_to_token(i,sep_ends[i]+1,1)
+                
+                y[i, start_tok : end_tok] = 1 
+
+            out1 = token_importances_extractor.forward(inputs.input_ids,
+                            inputs.attention_mask)
+
+            #t2=time.time()
+
+            forcing = 0.5 + np.cos(np.pi*(epoch*len(train_dataloader)+batch_idx)/tot_steps)/2
+            importances = forcing*y + (1-forcing)*out1
+
+            out2 = encoder_decoder(input_ids = inputs.input_ids,
+                         labels = labels.input_ids,
+                         token_importances = importances)
+            
+            #t3=time.time()
+
+            loss1 = loss_func_tokenImportancesExtractor(out1,y)
+            loss2 = out2.loss
+            loss = loss1 + loss2
+
+            #t4=time.time()
+
+            loss.backward()
+
+
+
+            if batch_idx % steps_per_update == steps_per_update-1:
+                if isinstance(steps_per_update, float):
+                    steps_per_update=int(steps_per_update*len(train_dataloader))
+                optimizer.step()
+                optimizer.zero_grad()
+
+            if steps_empty_cache is not None:
+                if isinstance(steps_empty_cache, float):
+                    steps_empty_cache=int(steps_empty_cache*len(train_dataloader))
+                if batch_idx % steps_empty_cache == steps_empty_cache-1:
+                    torch.cuda.empty_cache()
+
+            if steps_validate is not None:
+                if isinstance(steps_validate, float):
+                    steps_validate=int(steps_validate*len(train_dataloader))
+                if batch_idx % steps_validate == steps_validate-1:
+                    torch.cuda.empty_cache()
+
+                    val_l1, val_l2 = loss_validate( tokenizer, token_importances_extractor, encoder_decoder, val_dataloader, disp,
+                                                    use_history=use_history, device=device)
+
+                    val_loss_history.append([len(loss_history), val_l1.detach().cpu().numpy(), val_l2.detach().cpu().numpy()])
+                    torch.cuda.empty_cache()
+                    
+            running_loss1 += loss1.item()
+            running_loss2 += loss2.item()
+
+            loss_history.append([loss1.detach().cpu().numpy(), loss2.detach().cpu().numpy()])
+            
+            epoch_time = time.time() - start_time
+            batch_time = epoch_time/(batch_idx+1)
+            
+
+            disp.update(f"epoch: {epoch + 1}/{epochs}, {batch_idx + 1}/{len(train_dataloader)}, {epoch_time:.0f}s {batch_time*1e3:.0f}ms/step, lr: {optimizer.param_groups[0]['lr']:.3g}, forcing={forcing:.3g}, loss: {running_loss1/batch_steps:.3g} {running_loss2/batch_steps:.3g}")
+
+            if steps_save is not None:
+                if isinstance(steps_save, float):
+                    steps_save=int(steps_save*len(train_dataloader))
+                if batch_idx % steps_save == steps_save-1:
+                    torch.cuda.empty_cache()
+                    torch.save(model.state_dict(), f'{folder_name}\\{model_name}.pt')
+                    torch.save(optimizer.state_dict(), f'{folder_name}\\{model_name}_optimizer')
+                    np.save(f'{folder_name}/{model_name}_loss_history.npy', np.array(loss_history))
+                    np.save(f'{folder_name}/{model_name}_val_loss_history.npy', np.array(val_loss_history))
+                    torch.cuda.empty_cache()
+    torch.cuda.empty_cache()
+    torch.save(model.state_dict(), f'{folder_name}\\{model_name}.pt')
+    torch.save(optimizer.state_dict(), f'{folder_name}\\{model_name}_optimizer')
+    np.save(f'{folder_name}/{model_name}_loss_history.npy', np.array(loss_history))
+    np.save(f'{folder_name}/{model_name}_val_loss_history.npy', np.array(val_loss_history))
+    torch.cuda.empty_cache()
+
+
 def train_(train_dataloader, val_dataloader, model, model_name, use_history=False,
             epochs=3, learning_rate=5e-5, optimizer=None, 
             steps_per_update=1, steps_empty_cache=None, steps_validate=None,
@@ -391,7 +563,7 @@ def train_(train_dataloader, val_dataloader, model, model_name, use_history=Fals
 
             out2 = encoder_decoder(input_ids = inputs.input_ids,
                          labels = labels.input_ids,
-                         token_importances = y)
+                         token_importances = importances)
             
             t3=time.time()
 
@@ -448,8 +620,6 @@ def train_(train_dataloader, val_dataloader, model, model_name, use_history=Fals
     torch.save(model.state_dict(), file_path)
 
     return loss_history, val_loss_history, optimizer
-
-
 
 def loss_validate(  tokenizer, token_importances_extractor, encoder_decoder, val_dataloader, disp,
                     use_history = False, device='cpu'):
